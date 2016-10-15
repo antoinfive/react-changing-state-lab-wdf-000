e8dcfa6160af1c96fadc66fdacb5a41c533d9fb2
antoinfive <antoinfive@gmail.com>
2016-10-15 13:56:18 -0400

    Done.

--- components/Board.js
+++ components/Board.js
@@ -2,10 +2,32 @@ const React = require('react');
 const Field = require('./Field');
 
 class Board extends React.Component {
+  constructor(props){
+    super()
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleOnClick(index){
+    console.log(index)
+    return this.props.onClick(index)
+  }
+  
   render () {
-    const { board, onClick } = this.props;
+    const { board, onClick, turn } = this.props;
+    const fields = board.map((field, index) => {
+      debugger;
+      return (
+        <ul>
+          <li>
+            <Field key={index} onClick={onClick.bind(null, index)} player={field}/>
+          </li>
+        </ul>
+      )
+    })
+
     return (
-      <div>
+      <div className="board">
+        {fields}
       </div>
     );
   }
--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,59 @@ const solutions = require('./solutions');
 class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board:[null, null, null, null, null, null, null, null, null],
+      turn: 'X'
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.changeTurn = this.changeTurn.bind(this)
   }
 
   handleReset (ev) {
+    ev.preventDefault()
+    this.setState({
+      board:[null, null, null, null, null, null, null, null, null],
+      turn: 'X'
+    })
   }
-
+  
+  changeTurn(char){
+    return (char == "X" ? "O" : "X")
+  }
+  
   handleClick (i, ev) {
+    ev.preventDefault()
+    let newBoard = Object.assign([], this.state.board)
+    newBoard[i] = this.state.turn
+    this.setState({
+      board: newBoard,
+      turn: this.changeTurn(this.state.turn)
+    })
+ 
   }
 
   getWinner () {
+    const matches = solutions.map( (solutionRow) => ( 
+      solutionRow.map((index) => this.state.board[index]).join('')
+    ))
+  
+   const check = matches.find( (match) => match == 'XXX' || match == 'OOO')
+
+    return check && check[0]
   }
 
   isComplete () {
+    return this.state.board.every((field) => field) 
   }
 
   render () {
     return (
-      <div>
+      <div className="game">
+        <Board board={this.state.board} onClick={this.handleClick} turn={this.state.turn}/>
+        {this.isComplete() ? <Status winner={this.getWinner()} /> : null }
+        <button className='game__reset' onClick={this.handleReset} />
       </div>
     );
   }
